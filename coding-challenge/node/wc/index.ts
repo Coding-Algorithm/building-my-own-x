9c7e7681fdaa93e510dfbb97f95e95684c7d89bf
Ibrahim Taofeek <taofeekibrahimope@gmail.com>
2024-07-25 05:39:32 +0100

    working on accepting input from terminal

--- coding-challenge/node/wc/index.ts
+++ coding-challenge/node/wc/index.ts
@@ -1,54 +1,114 @@
 const fs = require("fs");
+const readLine = require("node:readline");
 
 const text = async () => {
   const flagOptions = ["-c", "-l", "-w", "-m"];
 
-  const flag = process.argv[2];
-  const filePath = process.argv[3];
-
-  if (!flag) {
-    console.log("Flag was not provided");
-    return;
-  }
+  const flag = process.argv[2].includes("-") ? process.argv[2] : null;
+  const filePath = !process.argv[2].includes("-")
+    ? process.argv[2]
+    : process.argv[3];
 
   if (flag && !flagOptions.includes(flag)) {
     console.log("Flag is not recognized");
     return;
   }
 
-  if (!process.argv[3]) {
-    console.log("File path was not provided");
-    return;
+  // if (!filePath) {
+  //   console.log("File path was not provided");
+  //   return;
+  // }
+
+  let fileString: string = "";
+  const isFile = fs.statSync(filePath ?? "").isFile();
+
+  if (!isFile) {
+    const fromTerminal = readLine.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+
+    fromTerminal.on("line", (line: string) => {
+      console.log(line, " .. ");
+      fileString += line;
+    });
+
+    if (!fileString) {
+      console.log("File provided is not valid");
+      return;
+    }
+  } else {
+    try {
+      fileString = await fs.readFileSync(filePath, "utf-8");
+    } catch (error: any) {
+      console.log(error.message);
+      return;
+    }
   }
 
-  try {
-    const isFile = fs.statSync(filePath).isFile();
+  const getFileLines = (file: any) => {
+    return new Promise((resolve, reject) => {
+      let lineCount = 0;
+      const fileStream = fs.createReadStream(file);
 
-    console.log("isFile", isFile);
-  } catch (error: any) {
-    console.log(error.message);
-    return;
-  }
+      fileStream.on("data", (chunk: Buffer) => {
+        for (let i = 0; i < chunk.length; ++i) {
+          if (chunk[i] === 10) {
+            lineCount++;
+          }
+        }
+      });
+
+      fileStream.on("end", () => {
+        resolve(lineCount);
+      });
+
+      fileStream.on("error", (error: any) => {
+        reject(error);
+      });
+    });
+  };
+
+  const getByte = (fileString: any) => {
+    return Buffer.byteLength(fileString, "utf-8");
+  };
+
+  const getWords = (fileString: any) => {
+    return fileString.split(/\s+/).filter((word: string) => word.length).length;
+  };
+
+  const getCharacters = (fileString: string) => {
+    return fileString.length;
+  };
+
+  return;
 
   switch (flag) {
     case "-c":
-      try {
-        const res = await fs.readFileSync("./text.txt", "utf-8");
-
-        const words = res.split(/\s+/).filter((word: string) => word.length);
-
-        console.log(words.length);
-      } catch (error: any) {
-        console.log(error.message);
-      }
+      console.log(getByte(fileString));
       break;
+
+    case "-w":
+      console.log(getWords(fileString));
+      break;
+
+    case "-m":
+      console.log(getCharacters(fileString));
+      break;
+
     case "-l":
+      getFileLines(filePath);
+      break;
+
+    case null:
+      getFileLines(filePath).then((res) =>
+        console.log(getByte(fileString), getWords(fileString), res, filePath)
+      );
       break;
 
     default:
       break;
   }
 };
-
 
 text();
